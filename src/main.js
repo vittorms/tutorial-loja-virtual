e685cdab547ad4620d07bbf52a2e0148bc8a5ddb
vittorms <vitor.msalomao@gmail.com>
2024-07-10 15:36:33 -0300

    Criação dos layouts e das stores.

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import './plugins/axios';
 
 const app = createApp(App)
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,7 @@
-<script setup></script>
+<script setup>
+import ProductList from '@/components/ProductList.vue';
+</script>
 
 <template>
-  <h1>Loja Virtual</h1>
+  <product-list />
 </template>
